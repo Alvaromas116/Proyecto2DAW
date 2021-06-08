c03048b1bf35a5b5239d35de6f4992d63bfb89db
Alvaromas116 <78785778+Alvaromas116@users.noreply.github.com>
2021-06-08 22:12:14 +0200

    todo hecho sin php

--- javascript/registrarse.js
+++ javascript/registrarse.js
@@ -41,16 +41,16 @@ window.addEventListener('load', function () {
     console.log("funciona")
     var imagenes = [];
 
-    imagenes[0] = "imagenes/Login/Slider/1.jpg"
-    imagenes[1] = "imagenes/Login/Slider/2.png"
-    imagenes[2] = "imagenes/Login/Slider/3.jpg"
-    imagenes[3] = "imagenes/Login/Slider/4.png"
-    imagenes[4] = "imagenes/Login/Slider/5.png"
-    imagenes[5] = "imagenes/Login/Slider/6.png"
-    imagenes[6] = "imagenes/Login/Slider/7.png"
-    imagenes[7] = "imagenes/Login/Slider/8.png"
-    imagenes[8] = "imagenes/Login/Slider/9.png"
-	imagenes[9] = "imagenes/Login/Slider/10.jpg"
+    imagenes[0] = "/./imagenes/Login/Slider/1.jpg"
+    imagenes[1] = "/./imagenes/Login/Slider/2.png"
+    imagenes[2] = "/./imagenes/Login/Slider/3.jpg"
+    imagenes[3] = "/./imagenes/Login/Slider/4.png"
+    imagenes[4] = "/./imagenes/Login/Slider/5.png"
+    imagenes[5] = "/./imagenes/Login/Slider/6.png"
+    imagenes[6] = "/./imagenes/Login/Slider/7.png"
+    imagenes[7] = "/./imagenes/Login/Slider/8.png"
+    imagenes[8] = "/./imagenes/Login/Slider/9.png"
+	imagenes[9] = "/./imagenes/Login/Slider/10.jpg"
 
 
 
