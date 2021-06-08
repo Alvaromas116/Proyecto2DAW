b82c471ff1fac95be29424f4f57ff6a57590813c
Alvaromas116 <78785778+Alvaromas116@users.noreply.github.com>
2021-06-08 02:10:50 +0200

    html y css del index

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Selecciona un item
+function selectItem(e) {
+	// Elimina todas las clases de show y border
+	removeBorder();
+	removeShow();
+	// Agrega borde al elemento de pestaña actual
+	this.classList.add('tab-border');
+	// Coge el elemento de contenido de DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Agrega la clase show
+	tabContentItem.classList.add('show');
+}
+
+// Borra todos los bordes de los items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Elimina el subrayado del item no seleccionado
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Selecciona el item cliqueado
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
