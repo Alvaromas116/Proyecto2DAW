1e863a32a1b15f1a62490304f221076e61522285
Alvaromas116 <78785778+Alvaromas116@users.noreply.github.com>
2021-06-08 20:47:30 +0200

    registrar sin php

--- javascript/registrarse.js
+++ javascript/registrarse.js
@@ -1,30 +1,8 @@
 const fila = document.querySelector('.contenedor-slider');
 const peliculas = document.querySelectorAll('.pelicula');
 
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
 
-// ? ----- ----- Event Listener para la flecha derecha. ----- -----
-flechaDerecha.addEventListener('click', () => {
-	fila.scrollLeft += fila.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if (indicadorActivo.nextSibling) {
-		indicadorActivo.nextSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
-});
-
-// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-flechaIzquierda.addEventListener('click', () => {
-	fila.scrollLeft -= fila.offsetWidth;
-
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if (indicadorActivo.previousSibling) {
-		indicadorActivo.previousSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
-});
 
 // ? ----- ----- Paginacion ----- -----
 const numeroPaginas = Math.ceil(peliculas.length / 5);
@@ -58,7 +36,37 @@ peliculas.forEach((pelicula) => {
 fila.addEventListener('mouseleave', () => {
 	peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
 });
+/*Con este script he creado el slider justo debajo del formulario*/
+window.addEventListener('load', function () {
+    console.log("funciona")
+    var imagenes = [];
 
+    imagenes[0] = "imagenes/Login/Slider/1.jpg"
+    imagenes[1] = "imagenes/Login/Slider/2.png"
+    imagenes[2] = "imagenes/Login/Slider/3.jpg"
+    imagenes[3] = "imagenes/Login/Slider/4.png"
+    imagenes[4] = "imagenes/Login/Slider/5.png"
+    imagenes[5] = "imagenes/Login/Slider/6.png"
+    imagenes[6] = "imagenes/Login/Slider/7.png"
+    imagenes[7] = "imagenes/Login/Slider/8.png"
+    imagenes[8] = "imagenes/Login/Slider/9.png"
+	imagenes[9] = "imagenes/Login/Slider/10.jpg"
+
+
+
+    var primeraImagen = 0;
+    var tiempo = 1000;
+
+    function cambiarImagenes() {
+        document.slider.src = imagenes[primeraImagen];
+        if (primeraImagen < 7) {
+            primeraImagen++;
+        } else {
+            primeraImagen = 0;
+        }
+    }
+    setInterval(cambiarImagenes, tiempo);
+});
 
 
 // ? ----- ----- FancyBox  BORRAR  ----- ----- 
@@ -80,4 +88,6 @@ $(document).ready(function () {
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 ga('create', 'UA-74824848-1', 'auto');
 ga('send', 'pageview');
+
+
 
