65ee4251cd3b005f183f7ecebd74e17bc82bc332
Alvaromas116 <78785778+Alvaromas116@users.noreply.github.com>
2021-06-09 04:24:12 +0200

    login y logout con php

--- login.php
+++ login.php
@@ -3,8 +3,57 @@
 require("conexion.php");
 session_start();
 
-?>
+$email = "";
+$pass = "";
 
+if (isset($_POST['login'])) {
+
+	$email = $_POST['email'];
+	$pass = $_POST['pass'];
+
+	$conexion = conectarBD();
+
+	$sql = "select count(*) from usuarios where email='$email' and pass='$pass'";
+	$res = $conexion->query($sql);
+	$array = $res->fetch_array();
+
+
+	$sql = "select distinct id_usr from usuarios where email='$email'";
+	$res = $conexion->query($sql);
+	$ID = $res->fetch_array();
+
+	$id = $ID[0];
+
+	$res = $conexion->query("select distinct nombre from usuarios where id_usr='$ID[0]'");
+	$nombre = $res->fetch_array();
+
+	$nom = $nombre[0];
+	$res->free();
+
+
+	if ($array[0] != 0) {
+
+		$_SESSION['email'] = $email;
+		$_SESSION['nombre'] = $nom;
+		$_SESSION['id'] = $id;
+
+		header("Location:index.php?p=peliculas");
+		exit;
+	} else {
+		
+		$log[3] = "Los datos son incorrectos";
+	}
+
+	if ($conexion->error != "") {
+		echo "Error: La ejecución de la consulta falló debido a: \n";
+		echo "Query: " . $sql . "<br>\n";
+		echo "Errno: " . $conexion->errno . "<br>\n";
+		echo "Error: " . $conexion->error . "<br>\n";
+		exit;
+	}
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,14 +76,29 @@ session_start();
 				<img src="imagenes/principal/pelismedia.png" class="logo">
 
 				<div class="login">
-					<form>
-						<h2>Iniciar sesión</h2>
 
-						<input type="text" name="name" id="name" placeholder="Introduce el correo electrónico"
-							autocomplete="off">
-						<input type="password" name="password" id="password" placeholder="Introduce la contraseña">
-						<input type="submit" value="Iniciar sesión">
+					<form action="index.php?p=login" method="post">
+
+						<h2>Iniciar sesión</h2>
+						<input type="text" name="email" value="<?php echo $email ?>" placeholder="Introduce el correo electrónico">
+
+						<input type="password" name="pass" value="<?php echo $pass ?>" placeholder="Introduce la contraseña">
+
+						<input type="submit" name="login" value="Iniciar sesión">
 					</form>
+
+					<?php
+					if (isset($_POST['login'])) {
+						if (!empty($log)) {
+							echo " <div id='errores'>";
+							foreach ($log as $clave => $value) {
+								echo $value . "<br>";
+							}
+							echo "</div>";
+						}
+					}
+					?>
+
 
 
 					<div class="footer-login">
@@ -57,54 +121,51 @@ session_start();
 			</div>
 
 			<div class="contenedor-principal">
-				<button role="button" id="flecha-izquierda" class="flecha-izquierda"><i
-						class="fas fa-angle-left"></i></button>
+				<button role="button" id="flecha-izquierda" class="flecha-izquierda"><i class="fas fa-angle-left"></i></button>
 
 
 				<div class="contenedor-slider">
 					<div class="slider">
 
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/1.jpg" data-fancybox data-caption="Valerian y la Ciudad de los Mil Planetas"><img src="imagenes/Login/Slider/1.jpg" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/1.jpg" data-fancybox data-caption="Valerian y la Ciudad de los Mil Planetas"><img src="imagenes/Login/Slider/1.jpg"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/2.png" data-fancybox data-caption="Interestelar"><img src="imagenes/Login/Slider/2.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/2.png" data-fancybox data-caption="Interestelar"><img src="imagenes/Login/Slider/2.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/3.jpg" data-fancybox data-caption="Terminator"><img src="imagenes/Login/Slider/3.jpg" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/3.jpg" data-fancybox data-caption="Terminator"><img src="imagenes/Login/Slider/3.jpg"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/4.png" data-fancybox data-caption="Top Gun"><img src="imagenes/Login/Slider/4.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/4.png" data-fancybox data-caption="Top Gun"><img src="imagenes/Login/Slider/4.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/5.png" data-fancybox data-caption="Tron: Legacy"><img src="imagenes/Login/Slider/5.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/5.png" data-fancybox data-caption="Tron: Legacy"><img src="imagenes/Login/Slider/5.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/6.png" data-fancybox data-caption="The Dark Knight"><img src="imagenes/Login/Slider/6.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/6.png" data-fancybox data-caption="The Dark Knight"><img src="imagenes/Login/Slider/6.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/7.png" data-fancybox data-caption="The Grey"><img src="imagenes/Login/Slider/7.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/7.png" data-fancybox data-caption="The Grey"><img src="imagenes/Login/Slider/7.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/8.png" data-fancybox data-caption="Marte"><img src="imagenes/Login/Slider/8.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/8.png" data-fancybox data-caption="Marte"><img src="imagenes/Login/Slider/8.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/9.png" data-fancybox data-caption="Ready Player One"><img src="imagenes/Login/Slider/9.png" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/9.png" data-fancybox data-caption="Ready Player One"><img src="imagenes/Login/Slider/9.png"></a>
 						</div>
 						<div class="pelicula">
-							<a class="fancybox" href ="imagenes/Login/Slider/10.jpg" data-fancybox data-caption="Mulan"><img src="imagenes/Login/Slider/10.jpg" ></a>
+							<a class="fancybox" href="imagenes/Login/Slider/10.jpg" data-fancybox data-caption="Mulan"><img src="imagenes/Login/Slider/10.jpg"></a>
 						</div>
 
 					</div>
 				</div>
 
-				<button role="button" id="flecha-derecha" class="flecha-derecha"><i
-						class="fas fa-angle-right"></i></button>
+				<button role="button" id="flecha-derecha" class="flecha-derecha"><i class="fas fa-angle-right"></i></button>
 			</div>
 		</div>
 	</main>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	<script src="javascript/jquery.fancybox.min.js"></script>
 	<script src="javascript/login.js"></script>
 </body>
