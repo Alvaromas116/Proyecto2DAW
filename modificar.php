fff98e70ff1b30dc4f563491ffdc716578e7d558
Alvaromas116 <78785778+Alvaromas116@users.noreply.github.com>
2021-06-09 22:19:40 +0200

    todo funcionando

--- modificar.php
+++ modificar.php
@@ -59,8 +59,7 @@ if (isset($_POST['modificar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/mi_estilo.css?v=<?php echo(rand()); ?>" />
-    <script src="/js/mi_script.js?v=<?php echo(rand()); ?>"></script>
+    <link rel="stylesheet" href="/css/modificar.css?v=<?php echo(rand()); ?>" />
     <link href="fontawesome/css/all.css" rel="stylesheet">
     <link rel="icon" sizes="32x32" href="imagenes/principal/p.png">
     <link rel="stylesheet" href="css/registrarse.css">
@@ -120,9 +119,6 @@ if (isset($_POST['modificar'])) {
     </header>
    
 
-    <main>
-
-    </main>
     <script src="javascript/registrarse.js"></script>
 </body>
 
--- peliculas.php
+++ peliculas.php
@@ -35,61 +35,62 @@ if (isset($_SESSION['id'])) {
 <body>
     <header>
         <div class="vista-top">
-        <a href="<?php echo $_SERVER['PHP_SELF'] ?>?p=principal"><img src="imagenes/principal/pelismedia.png" /></a>
-        <nav>
-    <ul>
-        <?php
-        if (isset($_SESSION['email'])) {
-            echo "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=logout>Cerrar Sesión</a></li>";
-            echo "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=modificar>Modificar Usuario</a></li>";
-        } else {
-            echo  "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=login>Iniciar Sesión</a></li>";
-            echo "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=principal>Volver al Inicio</a></li>";
-        }
-
-        ?>
-
-    </ul>
-</nav>
-<?php
-        if (isset($_SESSION['email'])) {
-
-            echo "<h1>Bienvenido " . $nombre . "</h1>";
-            echo "<h2></h2>";
-        } else {
-            echo "<h1>Desconectado. Haz LOGIN para chatear.</h1>";
-            echo "<h2></h2>";
-        }
-
-        ?>
 
             <form id="form">
+
                 <input type="text" placeholder="Buscar" id="buscar" class="buscar">
+
             </form>
+            <img src="imagenes/principal/pelismedia.png" />
+            <nav>
+                <ul>
+                    <?php
+                    if (isset($_SESSION['email'])) {
+                        echo "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=logout>Cerrar Sesión</a></li>";
+                        echo "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=modificar>Modificar Usuario</a></li>";
+                    } else {
+                        echo  "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=login>Iniciar Sesión</a></li>";
+                        echo "<li><a href=" . $_SERVER['PHP_SELF'] . "?p=principal>Volver al Inicio</a></li>";
+                    }
+
+                    ?>
+
+                </ul>
+            </nav>
+
+
+
         </div>
     </header>
     <div id="tags"></div>
     <div id="myNav" class="overlay">
 
-        <!-- Botón que cierra el trailer -->
+
+
+        <!-- Botón que cierra el trailer */ -->
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 
         <!-- Overlay del trailer -->
-        <div class="overlay-content" id="overlay-content"></div>
+        <div class="overlay-content" id="overlay-content"></div>';
 
         <a href="javascript:void(0)" class="arrow left-arrow" id="left-arrow">&#8656;</a>
 
         <a href="javascript:void(0)" class="arrow right-arrow" id="right-arrow">&#8658;</a>
 
-    </div>
-    <main id="main"></main>
-    <div class="pagination">
-        <div class="page" id="prev">Página anterior</div>
-        <div class="current" id="current">1</div>
-        <div class="page" id="next">Página siguiente</div>
 
     </div>
-
+    <?php
+    if (isset($_SESSION['email'])) {
+        echo '<main id="main"></main>';
+        echo '<div class="pagination">';
+        echo '<div class="page" id="prev">Página anterior</div>';
+        echo '<div class="current" id="current">1</div>';
+        echo '<div class="page" id="next">Página siguiente</div>';
+        echo '</div>';
+    } else {
+        echo '<h1> DEBES INICIAR SESIÓN PARA VER LOS TRAILERS';
+    }
+    ?>
     <script src="javascript/peliculas.js"></script>
 </body>
 
